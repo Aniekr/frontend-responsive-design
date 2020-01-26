4e1e524f3e9e06d0881b9611bc28ea6257ceb026
Aniekr <57792206+Aniekr@users.noreply.github.com>
2020-01-26 23:07:22 +0100

    Media queries toegevoegd, dropdown aangepast

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,24 @@
 /* function om zoekbalk te laten zien gebaseerd op of deze momenteel zichtbaar is
 hier heeft Joost Flick mij bij geholpen */
 function showSearch () {
-	const searchfield = document.getElementById('zoeken')
-	if (searchfield.style.display === 'none') {
-		searchfield.style.display = 'block'
-	}  else  {
-		searchfield.style.display = 'none'
-	}
+	console.log('search')
+	const searchfield = document.getElementsByClassName('zoeken')[0]
+	searchfield.classList.toggle("show")
 }
+
+/* toggle filter wel/niet laten zien
+https://www.w3schools.com/howto/howto_js_filter_dropdown.asp
+ */
+function openDropdown(type) {
+	document.getElementById(type).classList.toggle("show");
+}
+
 
 /* meerdere verhalen tegelijk selecteren */
 const articles = document.getElementsByClassName("story-title")
 Array.from(articles).forEach(function (story) {
 	story.addEventListener("click", function(){
-console.log(this) 
+console.log(this)
 		if (this.classList.contains('selected')) {
 			this.classList.remove('selected')
 		} else {
@@ -23,26 +28,3 @@ console.log(this)
 	)
 }
 )
-
-/* toggle filter wel/niet laten zien
-https://www.w3schools.com/howto/howto_js_filter_dropdown.asp
- */
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
-	var input, filter, ul, li, a, i;
-	input = document.getElementById("myInput");
-	filter = input.value.toUpperCase();
-	div = document.getElementById("myDropdown");
-	a = div.getElementsByTagName("a");
-	for (i = 0; i < a.length; i++) {
-	  txtValue = a[i].textContent || a[i].innerText;
-	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
-		a[i].style.display = "";
-	  } else {
-		a[i].style.display = "none";
-	  }
-	}
-  }
