9de76a127953f8c227c4e19a6a99cd149de81574
Aniekr <57792206+Aniekr@users.noreply.github.com>
2020-01-26 17:15:42 +0100

    dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -23,3 +23,26 @@ console.log(this)
 	)
 }
 )
+
+/* toggle filter wel/niet laten zien
+https://www.w3schools.com/howto/howto_js_filter_dropdown.asp
+ */
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+	var input, filter, ul, li, a, i;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	div = document.getElementById("myDropdown");
+	a = div.getElementsByTagName("a");
+	for (i = 0; i < a.length; i++) {
+	  txtValue = a[i].textContent || a[i].innerText;
+	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		a[i].style.display = "";
+	  } else {
+		a[i].style.display = "none";
+	  }
+	}
+  }
